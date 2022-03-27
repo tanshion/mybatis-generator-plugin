f2e9050bbc97ff3bd4c1acbf83bbcce26ac50648
tanshion <405781027@qq.com>
2022-03-27 19:48:05 +0800

    feat(类型转换): MyTypeResolverSolver

--- src/main/java/com/itfsw/mybatis/generator/plugins/ext/MyTypeResolverSolver.java
+++ src/main/java/com/itfsw/mybatis/generator/plugins/ext/MyTypeResolverSolver.java
@@ -11,27 +11,37 @@ import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Types;
+import java.util.regex.Pattern;
 
 public class MyTypeResolverSolver extends JavaTypeResolverDefaultImpl {
     protected static final Logger logger = LoggerFactory.getLogger(MyTypeResolverSolver.class);
-    public static final String KEY_TINYINT = "TINYINT";
-    public static final String KEY_SMALLINT = "SMALLINT";
-    public static final String KEY_NUMBER_DEFAULT = "NUMBER_DEFAULT";
-    public static final String NUMBER_LENGTH = "NUMBER_LENGTH";
-    public static final String SEQ_TYPE = "SEQ_TYPE";
-    public static final String SEQ_END_STR = "SEQ_END_STR";
+
+
+    // <property name="mapper_integer_type" value="TINYINT,SMALLINT,NUMBER,NUMBER38"/>
+    public static final String MAPPER_INTEGER_TYPE = "mapper_integer_type";
+    // <property name="mapper_long_type" value="number19,NUMBER20"/>
+    public static final String MAPPER_LONG_TYPE = "mapper_long_type";
+    // <property name="mapper_integer_regex" value="(?i)[\w]+(page$|page_no$)"/>
+    public static final String MAPPER_INTEGER_REGEX = "mapper_integer_regex";
+    // <property name="mapper_long_regex" value="(?i)[\w]+(seq$|seq_no$)"/>
+    public static final String MAPPER_LONG_REGEX = "mapper_long_regex";
 
     @Override
     protected FullyQualifiedJavaType overrideDefaultType(IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
         FullyQualifiedJavaType answer = super.overrideDefaultType(column, defaultType);
         logger.info("overrideDefaultType --> ActualColumnName=={},JdbcTypeName=={},JdbcType=={},Scale={},Length=={}",
                 column.getActualColumnName(), column.getJdbcTypeName(), column.getJdbcType(), column.getScale(), column.getLength());
-        if (column.getJdbcType() == Types.TINYINT && properties.containsKey(KEY_TINYINT)) {
-            answer = new FullyQualifiedJavaType(properties.getProperty(KEY_TINYINT));
+        if (properties.containsKey(MAPPER_INTEGER_TYPE)) {
+            String typeStr = properties.getProperty(MAPPER_INTEGER_TYPE).toUpperCase();
+            if (typeStr.contains(column.getJdbcTypeName())) {
+                answer = new FullyQualifiedJavaType("java.lang.Integer");
+            }
         }
-        if (column.getJdbcType() == Types.SMALLINT && properties.containsKey(KEY_SMALLINT)) {
-            answer = new FullyQualifiedJavaType(properties.getProperty(KEY_SMALLINT));
+        if (properties.containsKey(MAPPER_LONG_TYPE)) {
+            String typeStr = properties.getProperty(MAPPER_LONG_TYPE).toUpperCase();
+            if (typeStr.contains(column.getJdbcTypeName())) {
+                answer = new FullyQualifiedJavaType("java.lang.Long");
+            }
         }
         return answer;
     }
@@ -42,33 +52,42 @@ public class MyTypeResolverSolver extends JavaTypeResolverDefaultImpl {
             return defaultType;
         }
 
-        if (properties.containsKey(SEQ_TYPE) && properties.containsKey(SEQ_END_STR)) {
-            String[] endStrArr = properties.getProperty(SEQ_END_STR).split(",");
-            for (String endStr : endStrArr) {
-                if (column.getActualColumnName().endsWith(endStr)) {
-                    return new FullyQualifiedJavaType(properties.getProperty(SEQ_TYPE));
-                }
+        if (properties.containsKey(MAPPER_INTEGER_REGEX)) {
+            String pattern = properties.getProperty(MAPPER_INTEGER_REGEX);
+            if (Pattern.matches(pattern, column.getActualColumnName())) {
+                return new FullyQualifiedJavaType("java.lang.Integer");
             }
         }
 
-        if (properties.containsKey(NUMBER_LENGTH) && properties.getProperty(NUMBER_LENGTH).contains(column.getLength() + ",")) {
-            String[] nlProperty = properties.getProperty(NUMBER_LENGTH).split(",");
-            if (nlProperty.length == 2 && nlProperty[0].equals("" + column.getLength())) {
-                return new FullyQualifiedJavaType(nlProperty[1]);
+        if (properties.containsKey(MAPPER_LONG_REGEX)) {
+            String pattern = properties.getProperty(MAPPER_LONG_REGEX);
+            if (Pattern.matches(pattern, column.getActualColumnName())) {
+                return new FullyQualifiedJavaType("java.lang.Long");
             }
         }
 
-        if (column.getLength() > 18 || forceBigDecimals) {
-            return defaultType;
-        } else if (column.getLength() > 0 && column.getLength() <= 9) {
+        if (properties.containsKey(MAPPER_INTEGER_TYPE)) {
+            String typeStr = properties.getProperty(MAPPER_INTEGER_TYPE).toUpperCase() + ",";
+            int length = column.getLength();
+            String type = length == 0 ? "NUMBER," : "NUMBER" + length + ",";
+            if (typeStr.contains(type)) {
+                return new FullyQualifiedJavaType("java.lang.Integer");
+            }
+        }
+
+        if (properties.containsKey(MAPPER_LONG_TYPE)) {
+            String typeStr = properties.getProperty(MAPPER_INTEGER_TYPE).toUpperCase() + ",";
+            int length = column.getLength();
+            String type = length == 0 ? "NUMBER," : "NUMBER" + length + ",";
+            if (typeStr.contains(type)) {
+                return new FullyQualifiedJavaType("java.lang.Long");
+            }
+        }
+
+        if (column.getLength() > 0 && column.getLength() <= 9) {
             return new FullyQualifiedJavaType(Integer.class.getName());
-        } else {
-            if (properties.containsKey(KEY_NUMBER_DEFAULT)) {
-                return new FullyQualifiedJavaType(properties.getProperty(KEY_NUMBER_DEFAULT));
-            } else {
-                return new FullyQualifiedJavaType(Long.class.getName());
-            }
         }
+        return defaultType;
     }
 
 }
